87b8512944f9f068ff1c1266d334e4887ddb5dd8
Aidan Jaramillo <aidanjaramillo0326@gmail.com>
2025-12-30 17:13:25 -0600

    Added Task Functionality

---
+++ ViewModels/TaskViewModel.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+class TaskViewModel: ObservableObject {
+    @Published var tasks: [Task] = UserDefaults.standard.loadTasks() ?? [] {
+        didSet {
+            UserDefaults.standard.saveTasks(tasks)
+        }
+    }
+    
+    func addTask(name: String, course: String, dueDate: Date, priority: Priority){
+        let task = Task(name: name, course: course, dueDate: dueDate, priority: priority)
+        tasks.append(task)
+    }
+    
+    func toggleTask(task: Task){
+        if let index = tasks.firstIndex(where: { $0.id == task.id}){
+            tasks[index].isDone.toggle()
+        }
+    }
+    
+    func deleteTask(at offsets: IndexSet){
+        tasks.remove(atOffsets: offsets)
+    }
+}
+
+extension UserDefaults{
+    private static let tasksKey = "student_tasks"
+    
+    func saveTasks(_ tasks: [Task]) {
+        if let encoded = try? JSONEncoder().encode(tasks) {
+            set(encoded, forKey: UserDefaults.tasksKey)
+        }
+    }
+    
+    func loadTasks() -> [Task]? {
+        if let data = data(forKey: UserDefaults.tasksKey),
+           let decoded = try? JSONDecoder().decode([Task].self, from: data) {
+            return decoded
+        }
+        return nil
+    }
+}
